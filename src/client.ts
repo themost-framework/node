1bda78ac7bcdc325305292f45d9be19d395fa03c
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-10-23 08:31:45 +0000

    update license

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,4 @@
-/**
- * MOST Web Framework 2.0 Codename Blueshift
- * Copyright (c) 2017, THEMOST LP All rights reserved
- *
- * Use of this source code is governed by an BSD-3-Clause license that can be
- * found in the LICENSE file at https://themost.io/license
- */
+// MOST Web Framework 2.0 Codename Blueshift Copyright (c) 2017-2021, THEMOST LP All rights reserved
 import fetch from 'node-fetch';
 import {ClientDataContextOptions, ClientDataContext, ClientDataService, EdmSchema} from '@themost/client';
 import {Args, ResponseError} from '@themost/client';
